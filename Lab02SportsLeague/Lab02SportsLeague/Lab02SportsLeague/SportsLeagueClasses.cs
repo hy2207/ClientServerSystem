acedb89229b585b9cba36cdd5c695cb0533c9371
hy2207 <hy3980@gmail.com>
2020-09-23 19:36:05 -0700

    second lab

---
+++ Lab02SportsLeague/Lab02SportsLeague/Lab02SportsLeague/SportsLeagueClasses.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Lab02SportsLeague
+{
+    // Insert classes and enum here
+    #region Participants class
+    /// <summary>
+    /// participant abstract class - base class for anyone on a team, can't be instantiated
+    /// </summary>
+    public abstract class Participant : IComparable<Participant>
+    {
+        //fields
+        private static int nParticipants = 0; //used to keep a count of participants
+
+        //constructor
+        public Participant(string firstName, string lastName, int age)  
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+
+            nParticipants++; //increase the number of participants
+        }
+        #region Participant properties
+        public int Age
+        {
+            get; set;
+        }
+        public string FirstName
+        {
+            get; set;
+        }
+        public string LastName
+        {
+            get; set;
+        }
+        #endregion
+        //method
+        public static int GetNumberOfParticipants()
+        {
+            return nParticipants;
+        }
+
+        public int CompareTo(Participant p)
+        {
+            return this.LastName.CompareTo(p.LastName);
+        }
+        
+    }
+    #endregion
+    #region Athlete class
+    class Athlete : Participant
+    {
+        public Athlete(string firstName, string lastName, string position, int age)
+            : base(firstName, lastName, age)
+        {
+            Position = position;
+        }
+        public string Position
+        {
+            get; set;
+        }
+    }
+    #endregion
+    #region Coach class
+    public class Coach : Participant
+    {
+        public Coach(string firstName, string lastName, int age, Sport canCoachSport)
+            : base(firstName, lastName, age)
+        {
+            CanCoachSport = canCoachSport;
+        }
+        public Sport CanCoachSport
+        {
+            get; set;
+        }
+        
+    }
+    #endregion
+    //enum
+    public enum Sport
+    {
+        HOCKEY, BASKETBALL, SOCCER
+    }
+    
+    //team class
+    class Team
+    {
+        //field
+        private int teamSize = 0;
+        private Athlete[] athletes;
+        //constructor
+        public Team() { }
+        public Team(string name,Sport sport, int maxTeamSize)
+        {
+            TeamName = name;
+            TeamSport = sport;
+            MaxTeamSize = maxTeamSize;
+
+            athletes = new Athlete[maxTeamSize];
+        }
+        #region Team class properties
+        public Coach TeamCoach
+        {
+            get; private set;
+            //only team class obj can set it 
+        }
+        public Sport TeamSport
+        {
+            get; set;
+        }
+        public string TeamName
+        {
+            get; set;
+        }
+        public int TeamSize
+        {
+            get { return teamSize;  }
+            set
+            {
+                if (value > MaxTeamSize)
+                    WriteLine("Team size must be lower than " + MaxTeamSize);
+                else
+                    teamSize = value;
+            }
+        }
+        public int MaxTeamSize
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Team class methods
+        public bool AddAthlete(Athlete a)
+        {
+            if(TeamSize < MaxTeamSize)
+            {
+                athletes[teamSize] = a;
+                teamSize++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public void AddCoach(Coach c)
+        {
+            if (c.CanCoachSport == TeamSport)
+            {
+                TeamCoach = c;
+            }
+        }
+
+        public void DisplayTeam()
+        {
+            Array.Sort(athletes,0,teamSize); //sort array
+            //Array.Sort(athletes,0, teamSize, new SortRoster() ); // 2nd way
+
+            WriteLine($"\n======= {TeamSport} Team =======");
+            WriteLine($"Team Name: {TeamName} - {TeamSize} players ({MaxTeamSize} max)");
+            if (TeamCoach != null)
+            {
+                WriteLine($"Coach: {TeamCoach.FirstName} {TeamCoach.LastName} (age: {TeamCoach.Age}) (CanCoach {TeamCoach.CanCoachSport})");
+                WriteLine("\nTeam Roster\n");
+            }
+            else
+            {
+                WriteLine("Coach: None");
+            }
+            for(int i=0; i < athletes.Length; i++)
+            {
+                if (athletes[i] != null)
+                {
+                    WriteLine("{0} {1} {2} (age: {3})", athletes[i].Position, athletes[i].FirstName, athletes[i].LastName, athletes[i].Age);
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+            
+        }
+        #endregion
+
+        //2nd way sort rosters by last name
+        //class SortRoster
+        public class SortRoster : IComparer<Athlete>
+        {
+            public int Compare(Athlete athlete1, Athlete athlete2)
+            {
+                return athlete1.LastName.CompareTo(athlete2.LastName);
+            }
+        }
+
+    }
+}
+
---
+++ Lab02SportsLeague/Lab02SportsLeague/Lab02SportsLeague/SportsLeagueProgram.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab02SportsLeague
+{
+    // SportsLeague - Driver code for demonstrating inherited classes 
+    class SportsLeagueProgram
+    {
+        static void Main(string[] args)
+        {
+            // create four teams
+
+            Team mySoccerTeam = new Team("Kickers", Sport.SOCCER, 7);
+
+            Team[] hockeyTeams = new Team[2];
+
+            hockeyTeams[0] = new Team("Skaters", Sport.HOCKEY, 5);
+            hockeyTeams[1] = new Team("Shooters", Sport.HOCKEY, 5);
+
+
+            Team myBasketballTeam = new Team("Dribblers", Sport.BASKETBALL, 3);
+
+            // add 11 athletes and one coach to the soccer team, note it only has room for 7
+
+            mySoccerTeam.AddAthlete(new Athlete("Haspreet", "Kaur", "MidFielder", 19));
+            mySoccerTeam.AddAthlete(new Athlete("Kamaljeet", "Singh", "Forward", 22));
+            mySoccerTeam.AddAthlete(new Athlete("Mary", "Jones", "Goalkeeper", 23));
+            mySoccerTeam.AddAthlete(new Athlete("Jen", "Loi", "MidFielder", 19));
+            mySoccerTeam.AddAthlete(new Athlete("Dar", "Singh", "Forward", 22));
+            mySoccerTeam.AddAthlete(new Athlete("Sim", "Baker", "Defense", 23));
+            mySoccerTeam.AddAthlete(new Athlete("Tim", "Rauk", "MidFielder", 19));
+            mySoccerTeam.AddAthlete(new Athlete("Red", "Nites", "Forward", 22));
+            mySoccerTeam.AddAthlete(new Athlete("Mary", "Tines", "Goalkeeper", 23));
+            mySoccerTeam.AddAthlete(new Athlete("Maury", "Siguras", "Defense", 30));
+            mySoccerTeam.AddAthlete(new Athlete("Syd", "Calumphy", "Forward", 25));
+
+            mySoccerTeam.AddCoach(new Coach("Sonny", "Rooster", 41, Sport.SOCCER));
+
+            // add athletes and a coach to the first hockey team
+
+            hockeyTeams[0].AddAthlete(new Athlete("Jack", "Gretzky", "Centre", 19));
+            hockeyTeams[0].AddAthlete(new Athlete("John", "Crosby", "Forward", 22));
+            hockeyTeams[0].AddAthlete(new Athlete("Jim", "Luongo", "Goalie", 23));
+
+            hockeyTeams[0].AddCoach(new Coach("Alain", "Vineault", 50, Sport.HOCKEY));
+
+            // add an unqualified coach and no players to the second hockey team
+
+            hockeyTeams[1].AddCoach(new Coach("Mickey", "Mouse", 70, Sport.BASKETBALL));
+
+            // add athletes and a coach to the basketball team
+
+            myBasketballTeam.AddAthlete(new Athlete("Steph", "Curry", "Guard", 19));
+            myBasketballTeam.AddAthlete(new Athlete("Koby", "Jones", "Centre", 23));
+            myBasketballTeam.AddAthlete(new Athlete("Ken", "Bryant", "Forward", 22));
+
+            myBasketballTeam.AddCoach(new Coach("Jerry", "West", 72, Sport.BASKETBALL));
+
+            // show the number of participants in all sports
+
+            Console.WriteLine("All Sports: {0} participants", Participant.GetNumberOfParticipants());
+
+            // display each team
+
+            mySoccerTeam.DisplayTeam();
+            hockeyTeams[0].DisplayTeam();
+            hockeyTeams[1].DisplayTeam();
+            myBasketballTeam.DisplayTeam();
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
